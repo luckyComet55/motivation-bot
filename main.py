559154f58128821649ef38bed94ad38f1402ad51
Ivan Kurikhin <ivkurikhin@edu.hse.ru>
2023-08-09 20:48:40 +0300

    Bot v0.0.1

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 from dotenv import load_dotenv
+import logging
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 import os
 
-load_dotenv()
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
 
-def main():
-    print(os.getenv('API_KEY'))
+load_dotenv()
+api_key = os.environ['API_KEY']
+
+async def handleStart(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text='YOOOOOOO DO YOUR JOB MOZEFUCKAAAAA')
+
+
+def bot_initial_config(token: str):
+    return ApplicationBuilder().token(token).build()
 
 if __name__ == '__main__':
-    main()
+    app = bot_initial_config(api_key)
+    start_handler = CommandHandler('start', handleStart)
+    app.add_handler(start_handler)
+
+    app.run_polling()

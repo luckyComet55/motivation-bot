a3dc169d7b49d880f94f2a159abd6f698aad91bb
Ivan Kurikhin <ivkurikhin@edu.hse.ru>
2023-08-11 17:32:40 +0300

    Little refactoring

--- main.py
+++ main.py
@@ -62,12 +62,13 @@ async def handle_get_task(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 if __name__ == '__main__':
     app = config.app_config(api_key)
-    config.handlers_config(app,
-                           CommandHandler(strings.START, handle_start),
-                           MessageHandler(filters.TEXT & (~filters.COMMAND), handle_echo),
-                           CommandHandler(strings.ADD_ASSIGNMENT, handle_new_task),
-                           CommandHandler(strings.HELP, handle_help),
-                           CommandHandler(strings.GET_ASSIGNMENTS, handle_get_task)
-                           )
+    config.handlers_config(
+        app,
+        CommandHandler(strings.START, handle_start),
+        MessageHandler(filters.TEXT & (~filters.COMMAND), handle_echo),
+        CommandHandler(strings.ADD_ASSIGNMENT, handle_new_task),
+        CommandHandler(strings.HELP, handle_help),
+        CommandHandler(strings.GET_ASSIGNMENTS, handle_get_task)
+    )
 
     app.run_polling()
